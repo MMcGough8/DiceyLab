87499655abb5793d9badb32cb3ae4f19ccc771f1
mmcgough8 <mwm9490@icloud.com>
2025-11-09 17:36:41 -0500

    "Dice created and tests for d6, d20, d100"

--- java/src/main/java/Simulation.java
+++ java/src/main/java/Simulation.java
@@ -1,5 +1,38 @@
+
+
 public class Simulation {
 
+    public static void main(String[] args) {
+
+        Dice d6 = new Dice(6);
+        Dice d20 = new Dice(20);
+        Dice d100 = new Dice(100);
+
+        System.out.println("Rolling a 6-sided dice: " + d6.roll());
+        for (int i = 0; i < 5; i++) {
+            int roll = d6.roll();
+            System.out.println("Roll " + (i + 1) + ": " + roll);
+        }
+        System.out.println("Last roll remember: " + d6.getLastRoll());
+        System.out.println();
+
+   
+        System.out.println("Rolling a 20-sided dice again: " + d20.roll());
+        for (int i = 0; i < 5; i++) {
+            int roll = d20.roll();
+            System.out.println("Roll " + (i + 1) + ": " + roll);
+        }
+        System.out.println("Last roll remember: " + d20.getLastRoll());
+        System.out.println();
 
 
+        System.out.println("Rolling a 100-sided dice: " + d100.roll());
+        for (int i = 0; i < 5; i++) {
+            int roll = d100.roll();
+            System.out.println("Roll " + (i + 1) + ": " + roll);
+        }
+        System.out.println("Last roll remember: " + d100.getLastRoll());
+        System.out.println();
+    }
+        
 }
