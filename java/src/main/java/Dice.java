810c9485c05e77fa5d59f613b88a17dc15e5c11f
mmcgough8 <mwm9490@icloud.com>
2025-11-09 16:42:33 -0500

    "Initial Commit with dice code"

--- java/src/main/java/Dice.java
+++ java/src/main/java/Dice.java
@@ -1,4 +1,31 @@
+
+
+
+
 public class Dice {
+
+    private int sides;
+    private int lastRoll;
+
+    public Dice(int sides) {
+        this.sides = sides;        
+    }
+
+    public int roll() {
+        lastRoll = (int)(Math.random() * sides) + 1;
+        return lastRoll;
+    }
+
+    public int getLastRoll() {
+        return lastRoll;
+    }
+
+    public int getSides() {
+        return sides;
+    }
+
 
 
 }
+
+
