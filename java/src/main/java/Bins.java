c2691ef3812bf9da5163779f8e187372bdd9b1b3
mmcgough8 <mwm9490@icloud.com>
2025-11-09 18:18:30 -0500

    "Created bin class"

--- java/src/main/java/Bins.java
+++ java/src/main/java/Bins.java
@@ -1,10 +1,40 @@
 
 public class Bins {
 
+    private int[] bins;
+    private int  minValue;
+    private int  maxValue;
 
+    public Bins(int minValue, int maxValue) {
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        bins = new int[maxValue - minValue + 1];
+    }
+    public Integer getBin(int value) {
+        if (value < minValue && value > maxValue) {
+            return null;
+        }
+            int index = value - minValue;
+            return bins[index];
+    }
+    public void incrementBin(int value) {
+        if (value < minValue && value > maxValue) {
+            return;
+        }
+        int index = value - minValue;
+        bins[index]++;
+    }
 
+    public int getMinValue() {
+        return minValue;
+    }
 
-
-
-
+    public int getMaxValue() {
+        return maxValue;
+    }
+    public void clear() {
+        for (int i = 0; i < bins.length; i++) {
+            bins[i] = 0;
+        }
+    }   
 }
