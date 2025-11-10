c87ef9a01fb0e832b4bd4d4036f13a5f308abc52
mmcgough8 <mwm9490@icloud.com>
2025-11-09 19:10:35 -0500

    "Updated Simulation and ran formated simulation for (2, 1000000)

--- java/src/main/java/Simulation.java
+++ java/src/main/java/Simulation.java
@@ -2,37 +2,67 @@
 
 public class Simulation {
 
-    public static void main(String[] args) {
+        private int numDice;
+        private int numTrials;
+        private int sidesPerDie;
+        private Bins results;
 
-        Dice d6 = new Dice(6);
-        Dice d20 = new Dice(20);
-        Dice d100 = new Dice(100);
-
-        System.out.println("Rolling a 6-sided dice: " + d6.roll());
-        for (int i = 0; i < 5; i++) {
-            int roll = d6.roll();
-            System.out.println("Roll " + (i + 1) + ": " + roll);
-        }
-        System.out.println("Last roll remember: " + d6.getLastRoll());
-        System.out.println();
-
-   
-        System.out.println("Rolling a 20-sided dice again: " + d20.roll());
-        for (int i = 0; i < 5; i++) {
-            int roll = d20.roll();
-            System.out.println("Roll " + (i + 1) + ": " + roll);
-        }
-        System.out.println("Last roll remember: " + d20.getLastRoll());
-        System.out.println();
-
-
-        System.out.println("Rolling a 100-sided dice: " + d100.roll());
-        for (int i = 0; i < 5; i++) {
-            int roll = d100.roll();
-            System.out.println("Roll " + (i + 1) + ": " + roll);
-        }
-        System.out.println("Last roll remember: " + d100.getLastRoll());
-        System.out.println();
-    }
+       
+    public Simulation(int numDice, int numTrials) {
+        this.numDice = numDice;
+        this.numTrials = numTrials;
+        this.sidesPerDie = 6;
         
-}
+        int minValue = numDice;
+        int maxValue = numDice * sidesPerDie;
+        this.results = new Bins(minValue, maxValue);
+    }
+
+    public void runSimulation() {
+        System.out.println("Running simulation: " + numDice + "d" + sidesPerDie + " rolled " + numTrials + " times...\n");
+
+        Dice[] dice = new Dice[numDice];
+        for (int i = 0; i <  numDice; i++) {
+            dice[i] = new Dice(sidesPerDie);
+    }
+
+        for (int trial = 0; trial < numTrials; trial++) {
+            int total = 0;
+
+            for (int i = 0; i < numDice; i++) {
+                total += dice[i].roll();
+            }
+            results.incrementBin(total);
+        }
+    }
+
+    public void printResults() {
+        System.out.println("SIMULATION RESULTS");
+        System.out.println("==================");
+        System.out.println("Dice: " + numDice + "d" + sidesPerDie);
+        System.out.println("Trials: " + numTrials);
+        System.out.println();
+
+        System.out.println(" sum | Count | Percentage | Bar Chart ");
+        System.out.println(" ----+-------+------------+---------- ");
+
+        for (int sum = results.getMinValue(); sum <= results.getMaxValue(); sum++) {
+            int count = results.getBin(sum);
+            double percentage = (count * 100.0) / numTrials;
+
+            int barLength = (int) (percentage *2);
+            String bar = "*".repeat(barLength);
+
+            System.out.printf("%4d | %6d | %6.2f%%  | %s\n",
+                             sum, count, percentage, bar);
+        }
+    }   
+
+        public static void main(String[] args) {
+            Simulation sim = new Simulation(2,1000000);
+
+            sim.runSimulation();
+
+            sim.printResults();
+        }
+    }
